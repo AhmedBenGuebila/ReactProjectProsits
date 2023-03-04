f1162417add7c477fc5aae9b716e7d76f0bd5ba3
AhmedBenGuebila <ahmed.esprit@esprit.tn>
2023-03-04 15:04:05 +0100

    atelier4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
-import {React } from "react";
-import { Products } from './components/Products';
+import "./App.css";
+import { React } from "react";
+import { Products } from "./components/Products";
+import { Route, Routes } from "react-router-dom";
+import { NotFound } from "./components/NotFound";
+import { NavbarComponent } from "./components/Navbar";
+import { Detail_produit } from "./components/Detail-produit";
 
 function App() {
   return (
     <div>
-    <Products />
+      <NavbarComponent />
+      <Routes>
+        <Route path="">
+          <Route path="/products" element={<Products />}></Route>
+          <Route path="/detailproduit/:name" element={<Detail_produit />}></Route>
+
+        </Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
     </div>
   );
 }
